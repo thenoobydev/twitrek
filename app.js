5ceea06c1b62878e5ab947f820b8f0f9768609df
goelaakash79 <goelaakash79@gmail.com>
2018-10-13 02:20:05 +0530

    twitter search integrated

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Twitter = require('twitter');
+const config = require('./config/config.js');
+const twitter = new Twitter(config);
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+app.post('/api1', (req, res) => {
+  let params = {
+    q: '#' + req.body.query,
+    count: 10,
+    result_type: 'recent',
+    lang: 'en'
+  }
+  twitter.get('search/tweets', params, (err, data, response) => {
+    if(err) throw err;
+    // console.log(data);
+    data.statuses.forEach(function(info) {
+      let id = info.id_str;
+      let text = info.text;
+      let metadata = info.metadata;
+      console.log(text+"\n"+metadata+"\n\n")
+
+    });
+  });
+});
+
+app.listen(3000, () => console.log('Server Running.'));
